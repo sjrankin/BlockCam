5d20b76f78bab1b9b8d3b3f0eeb6d18fb0592186
Stuart Rankin <sjrankin@gmail.com>
2020-01-21 22:23:06 +0900

    Update Colors.swift
    Added a gradient for the composite status display background and for the scene recording control bar.

--- BlockCam/Utilities/Colors.swift
+++ BlockCam/Utilities/Colors.swift
@@ -53,4 +53,35 @@ class Colors
         Layer.colors = [UIColor.systemIndigo.cgColor, UIColor.black.cgColor]
         return Layer
     }
+    
+    /// Return a gradient to use with the SmallStatusDisplay background.
+    /// - Parameter Container: The bounds value for the small status display.
+    /// - Returns: `CAGradientLayer` to be used by the small status display.
+    public static func GetCompositeStatusGradient(Container Frame: CGRect) -> CAGradientLayer
+    {
+        let Layer = CAGradientLayer()
+        Layer.name = "GradientBackground"
+        Layer.zPosition = -1000
+        Layer.frame = Frame
+        let DarkIndigo = UIColor(red: 70.0 / 255.0, green: 65.0 / 255.0, blue: 190.0 / 255.0, alpha: 1.0)
+        Layer.colors = [UIColor.black.cgColor, DarkIndigo.cgColor]
+        Layer.locations = [NSNumber(value: 0.2), NSNumber(value: 1.0)]
+        return Layer
+    }
+    
+    /// Return a gradient to use with the scene recording bar background.
+    /// - Parameter Container: The bounds value for the scene recorder.
+    /// - Returns: `CAGradientLayer` to be used by the scene recorder.
+    public static func GetSceneRecordGradient(Container Frame: CGRect) -> CAGradientLayer
+    {
+        let Layer = CAGradientLayer()
+        Layer.name = "GradientBackground"
+        Layer.zPosition = -1000
+        Layer.frame = Frame
+        let Green1 = UIColor(red: 24.0 / 255.0, green: 220.0 / 255.0, blue: 60.0 / 255.0, alpha: 1.0)
+        let Green2 = UIColor(red: 52.0 / 255.0, green: 199.0 / 255.0, blue: 89.0 / 255.0, alpha: 1.0)
+        Layer.colors = [Green1.cgColor, Green2.cgColor]
+        Layer.locations = [NSNumber(value: 0.2), NSNumber(value: 1.0)]
+        return Layer
+    }
 }
