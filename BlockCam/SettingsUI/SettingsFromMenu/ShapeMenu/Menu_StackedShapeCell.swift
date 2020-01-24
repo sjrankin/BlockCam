814556f49d8881219343934d2526d79609677346
Stuart Rankin <sjrankin@gmail.com>
2020-01-24 22:29:35 +0900

    Create Menu_StackedShapeCell.swift
    Code to run the stacked shape UI.

---
+++ BlockCam/SettingsUI/SettingsFromMenu/ShapeMenu/Menu_StackedShapeCell.swift
@@ -0,0 +1,79 @@
+//
+//  Menu_StackedShapeCell.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 1/24/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Menu_StackedShapeCell: UITableViewCell
+{
+    public weak var Delegate: CompositeShapeChangeProtocol? = nil
+    
+    public static let CellHeight: CGFloat = 45.0
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?)
+    {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        InitializeUI()
+    }
+    
+    required init?(coder: NSCoder)
+    {
+        super.init(coder: coder)
+        InitializeUI()
+    }
+    
+    override var isEditing: Bool
+        {
+        get
+        {
+            return super.isEditing
+        }
+        set
+        {
+            super.isEditing = newValue
+        }
+    }
+    
+    override var showsReorderControl: Bool
+        {
+        get
+        {
+            return true
+        }
+        set
+        {
+            //Do nothing
+        }
+    }
+    
+    func InitializeUI()
+    {
+        TitleLabel = UILabel(frame: CGRect(x: 5, y: 5, width: 100, height: 35))
+        TitleLabel.font = UIFont.boldSystemFont(ofSize: 18.0)
+        TitleLabel.textColor = UIColor.black
+        contentView.addSubview(TitleLabel)
+    }
+    
+    var TitleLabel: UILabel!
+    
+    func Load(Title: String, TableWidth: CGFloat, Index: Int)
+    {
+        let ItemContextMenu = UIContextMenuInteraction(delegate: self)
+        self.addInteraction(ItemContextMenu)
+        ShapeIndex = Index
+        let TitleWidth = (TableWidth - 10) * 0.8
+        TitleLabel.frame = CGRect(x: TitleLabel.frame.minX,
+                                  y: TitleLabel.frame.minY,
+                                  width: TitleWidth,
+                                  height: TitleLabel.frame.height)
+        TitleLabel.text = Title
+        self.selectionStyle = .none
+    }
+    
+    var ShapeIndex = 0
+}
