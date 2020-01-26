61f5c2f38a598dfcc9b32dd74ad56c87150167fa
Stuart Rankin <sjrankin@gmail.com>
2020-01-26 21:59:55 +0900

    Create MenuCommandController.swift
    The code to handle the ContextMenuProtocol in ViewController - calls the proper function for the context menus.

---
+++ BlockCam/Menus/MenuCommandController.swift
@@ -0,0 +1,54 @@
+//
+//  MenuCommandController.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 1/26/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController: ContextMenuProtocol
+{
+    /// Handle commands from the various context menus.
+    /// - Parameter Command: The command issued by the user.
+    func HandleContextMenu(Command: ContextMenuCommands)
+    {
+        switch Command
+        {
+            case .Cancelled:
+                break
+            
+            case .CurrentImageSettings:
+                ShowCurrentSettings()
+            
+            case .ProgramSettings:
+                ChangeSettingsFromMenu()
+            
+            case .SetImageOptions:
+                RunShapeSettingsFromMenu()
+            
+            case .ShowAbout:
+                ShowAboutFromMenu()
+            
+            case .LightingOptions:
+                RunLightingSettingsFromMenu()
+            
+            case .PerformanceOptions:
+                RunPerformanceSettingsFromMenu()
+            
+            case .LoadScene:
+                LoadSavedScene()
+            
+            case .RecordScene:
+                ShowRecordSceneBar()
+            
+            case .SaveScene:
+                SaveScene()
+            
+            case .ShareImage:
+                RunExportProcessedImageFromMenu()
+        }
+    }
+}
