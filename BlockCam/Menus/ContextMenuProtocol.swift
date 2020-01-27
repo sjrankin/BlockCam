d103b1cded85508ec673355c73b0a2e7901a187a
Stuart Rankin <sjrankin@gmail.com>
2020-01-27 16:55:25 +0900

    Update ContextMenuProtocol.swift
    Added comments. Added .ShowHelp to enum.

--- BlockCam/Menus/ContextMenuProtocol.swift
+++ BlockCam/Menus/ContextMenuProtocol.swift
@@ -9,22 +9,39 @@
 import Foundation
 import UIKit
 
+/// Protocol for handling context menu commands.
 protocol ContextMenuProtocol: class
 {
+    /// Command sent from a context menu.
+    /// - Parameter Command: The actual command.
     func HandleContextMenu(Command: ContextMenuCommands)
 }
 
+/// Valid context menu commands.
 enum ContextMenuCommands: String, CaseIterable
 {
+    /// Context menu closed or cancelled without a command being issued.
     case Cancelled = "Cancelled"
+    /// Show the About dialog.
     case ShowAbout = "ShowAbout"
+    /// Show program settings.
     case ProgramSettings = "ProgramSettings"
+    /// Show current image settings (eg, the settings in effect at the time).
     case CurrentImageSettings = "CurrentImageSettings"
+    /// Run the image options dialog (mainly for shapes).
     case SetImageOptions = "SetImageOptions"
+    /// Run the lighting options dialog.
     case LightingOptions = "SetLightingOptions"
+    /// Run the performance options dialog.
     case PerformanceOptions = "SetPerformanceOptions"
+    /// Load a saved scene.
     case LoadScene = "LoadScene"
+    /// Save current processed view as a scene.
     case SaveScene = "SaveScene"
+    /// Record scene motion as a video.
     case RecordScene = "RecordScene"
+    /// Share the image via the share API.
     case ShareImage = "ShareImage"
+    /// Show help.
+    case ShowHelp = "ShowHelp"
 }
