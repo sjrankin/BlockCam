14c5609773472a44898831bf4a4b570179cb3a5f
Stuart Rankin <sjrankin@gmail.com>
2020-01-30 22:02:15 +0900

    Update Gyroscope.swift
    Removed Gryoscope-specific functions. Added StartOrientationUpdates.

--- BlockCam/+ViewController/Gyroscope.swift
+++ BlockCam/+ViewController/Gyroscope.swift
@@ -12,34 +12,63 @@ import CoreMotion
 
 extension ViewController
 {
-    func StartGyroscopeUpdates()
+    /// Start monitoring orientation updates. Updates to the rotation of the device trigger orientation
+    /// changes in some UI elements.
+    /// - Note: See [CMDeviceMotion](https://nshipster.com/cmdevicemotion/)
+    /// - Parameter UpdateFrequency. How often to check for changes. Defaults to 1/60th of a second.
+    func StartOrientationUpdates(_ UpdateFrequency: Double = 1.0 / 60.0)
     {
         MotionManager = CMMotionManager()
-        if MotionManager!.isGyroAvailable
+        if MotionManager!.isDeviceMotionAvailable
         {
-            MotionManager?.gyroUpdateInterval = 1.0 / 60.0
-        MotionManager?.startGyroUpdates()
-            let GyroTimer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true)
+            MotionManager?.deviceMotionUpdateInterval = UpdateFrequency
+            MotionManager?.startDeviceMotionUpdates(to: .main)
             {
-                (timer) in
-                if let GyroData = self.MotionManager?.gyroData
+                [weak self] (data, error) in
+                guard let MotionData = data, error == nil else
                 {
-                    print("RotationRate.z=\(GyroData.rotationRate.z)")
+                    return
+                }
+                var Rotation = atan2(MotionData.gravity.x, MotionData.gravity.y) - .pi
+                Rotation = Rotation * 180.0 / .pi
+                Rotation = abs(round(Rotation))
+                if self!.PreviousRotation == Rotation
+                {
+                    return
+                }
+                self!.PreviousRotation = Rotation
+                let GridType = Settings.GetEnum(ForKey: .LiveViewGridType, EnumType: GridTypes.self, Default: GridTypes.None)
+                if GridType != .None
+                {
+                    self!.GridView.DrawGrid(CGFloat(360.0 - Rotation))
+                }
+                //If the rotation is close to a cardinal direction, force it to the exact
+                //cardinal direction.
+                switch Rotation
+                {
+                    case 0.0 ... 15.0,
+                         345.0 ... 359.999:
+                        self!.UpdateButtonAngle(360.0 - 0.0)
+                    
+                    case 75.0 ... 105.0:
+                        self!.UpdateButtonAngle(360.0 - 90.0)
+                    
+                    case 165.0 ... 195.0:
+                        self!.UpdateButtonAngle(360.0 - 180.0)
+                    
+                    case 255.0 ... 285.0:
+                        self!.UpdateButtonAngle(360.0 - 270.0)
+                    
+                    default:
+                        self!.UpdateButtonAngle(360.0 - Rotation)
                 }
             }
-            RunLoop.current.add(GyroTimer, forMode: .default)
-        }
-        else
-        {
-            MotionManager = nil
         }
     }
     
-    func StopGyrocopeUpdates()
+    /// Stops the motion manager from updating rotational changes.
+    func StopOrientationUpdates()
     {
-        if MotionManager != nil
-        {
-            MotionManager?.stopGyroUpdates()
-        }
+        MotionManager?.stopDeviceMotionUpdates()
     }
 }
