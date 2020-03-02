775b3bf2cf4e8605b099f17552f7a6a2220f9c51
Stuart Rankin <sjrankin@gmail.com>
2020-03-02 20:54:11 +0900

    Update Extensions.swift
    Added ToImage for UIViews.

--- BlockCam/Utilities/Extensions.swift
+++ BlockCam/Utilities/Extensions.swift
@@ -645,6 +645,15 @@ extension UIView
         }
         return nil
     }
+    
+    func ToImage() -> UIImage
+    {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.isOpaque, 0.0)
+        self.drawHierarchy(in: self.bounds, afterScreenUpdates: false)
+        let SomeView = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return SomeView!
+    }
 }
 
 /// NSCharacterSet extension to return all characters in a font.
