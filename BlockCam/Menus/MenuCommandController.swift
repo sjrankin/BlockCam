8e64266499180969cfad093c495e2ffda39f9258
Stuart Rankin <sjrankin@gmail.com>
2020-03-03 20:44:54 +0900

    Update MenuCommandController.swift
    Minor code formatting.

--- BlockCam/Menus/MenuCommandController.swift
+++ BlockCam/Menus/MenuCommandController.swift
@@ -57,7 +57,7 @@ extension ViewController: ContextMenuProtocol
                 break
             
             case .ShowFavoriteShapes:
-            break
+                break
         }
     }
     
