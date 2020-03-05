e5d81765c54f8a0351df81bbe9782b1f49fa33a9
Stuart Rankin <sjrankin@gmail.com>
2020-03-05 20:55:15 +0900

    Update Versioning.swift
    Auto updated.

--- BlockCam/Versioning.swift
+++ BlockCam/Versioning.swift
@@ -115,19 +115,19 @@ public class Versioning: CustomStringConvertible
     }
     
     /// Build number.
-    public static let Build: Int = 2087
+    public static let Build: Int = 2095
     
     /// Build increment.
     private static let BuildIncrement = 1
     
     /// Build ID.
-    public static let BuildID: String = "72C2484B-6760-42D8-A159-19828D783B69"
+    public static let BuildID: String = "9431EB99-B783-4C4D-8157-73324106A89D"
     
     /// Build date.
-    public static let BuildDate: String = "4 March 2020"
+    public static let BuildDate: String = "5 March 2020"
     
     /// Build Time.
-    public static let BuildTime: String = "18:01"
+    public static let BuildTime: String = "16:11"
     
     /// Holds the release build flag.
     private static var _IsReleaseBuild: Bool = false
