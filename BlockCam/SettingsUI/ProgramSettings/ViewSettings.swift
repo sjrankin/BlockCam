2c8a0cf357a4d05be7cabe7f8ed0692a79eb33c4
Stuart Rankin <sjrankin@gmail.com>
2020-03-19 16:57:04 +0900

    Update ViewSettings.swift
    Removed histogram-related code.

--- BlockCam/SettingsUI/ProgramSettings/ViewSettings.swift
+++ BlockCam/SettingsUI/ProgramSettings/ViewSettings.swift
@@ -25,7 +25,6 @@ class ViewSettings: UITableViewController, UIPickerViewDelegate, UIPickerViewDat
             BestFitOffsetSegment.selectedSegmentIndex = 0
             Settings.SetDouble(0.0, ForKey: .BestFitOffset)
         }
-        ShowHistogramSwitch.isOn = Settings.GetBoolean(ForKey: .ShowHistogram)
         let InitialS = Settings.GetString(ForKey: .InitialView)
         if let Initial = InitialS == nil ? ProgramModes.LiveView : ProgramModes(rawValue: InitialS!)
         {
@@ -35,25 +34,6 @@ class ViewSettings: UITableViewController, UIPickerViewDelegate, UIPickerViewDat
         {
             ModeSegements.selectedSegmentIndex = 0
         }
-        if let RawOrder = Settings.GetString(ForKey: .HistogramOrder)
-        {
-            InitializeHistogramOrder(RawOrder)
-        }
-        else
-        {
-            Settings.SetString(HistogramOrders.RGB.rawValue, ForKey: .HistogramOrder)
-            InitializeHistogramOrder(HistogramOrders.RGB.rawValue)
-        }
-        if let RawSpeed = Settings.GetString(ForKey: .HistogramCreationSpeed)
-        {
-            InitializeHistogramSpeed(RawSpeed)
-        }
-        else
-        {
-            Settings.SetString(HistogramCreationSpeeds.Medium.rawValue, ForKey: .HistogramCreationSpeed)
-            InitializeHistogramSpeed(HistogramCreationSpeeds.Medium.rawValue)
-        }
-        ProcessedHistogramSwitch.isOn = Settings.GetBoolean(ForKey: .ShowProcessedHistogram)
         ShowCurrentOrientationSwitch.isOn = Settings.GetBoolean(ForKey: .ShowActualOrientation)
         if let RawGridType = Settings.GetString(ForKey: .LiveViewGridType)
         {
@@ -73,7 +53,6 @@ class ViewSettings: UITableViewController, UIPickerViewDelegate, UIPickerViewDat
             Settings.SetString(GridTypes.None.rawValue, ForKey: .LiveViewGridType)
             GridPicker.selectRow(0, inComponent: 0, animated: true)
         }
-        CombineHistogramColorsSwitch.isOn = Settings.GetBoolean(ForKey: .CombinedHistogram)
     }
     
     let ModeMap =
@@ -82,11 +61,6 @@ class ViewSettings: UITableViewController, UIPickerViewDelegate, UIPickerViewDat
             ProgramModes.PhotoLibrary: 1,
             ProgramModes.ProcessedView: 2
     ]
-    
-    @IBAction func HandleHistogramChanged(_ sender: Any)
-    {
-        Settings.SetBoolean(ShowHistogramSwitch.isOn, ForKey: .ShowHistogram)
-    }
     
     @IBAction func HandleModeChanged(_ sender: Any)
     {
@@ -137,98 +111,6 @@ class ViewSettings: UITableViewController, UIPickerViewDelegate, UIPickerViewDat
             8.0: 4
     ]
     
-    @IBAction func HandleHistogramOrderChanged(_ sender: Any)
-    {
-        if let Segment = sender as? UISegmentedControl
-        {
-            let NewIndex = Segment.selectedSegmentIndex
-            for (Order, Index) in OrderMap
-            {
-                if Index == NewIndex
-                {
-                    Settings.SetString(Order.rawValue, ForKey: .HistogramOrder)
-                }
-            }
-        }
-    }
-    
-    func InitializeHistogramOrder(_ Raw: String)
-    {
-        if Raw.isEmpty
-        {
-            Log.AbortMessage("Unexpectedly empty string passed to InitializeHistogramOrder")
-            {
-                Message in
-                fatalError(Message)
-            }
-        }
-        if let Order = HistogramOrders(rawValue: Raw)
-        {
-            if let OrderIndex = OrderMap[Order]
-            {
-                HistogramOrder.selectedSegmentIndex = OrderIndex
-                return
-            }
-        }
-        HistogramOrder.selectedSegmentIndex = 0
-        Settings.SetString(HistogramOrders.RGB.rawValue, ForKey: .HistogramOrder)
-    }
-    
-    let OrderMap =
-        [
-            HistogramOrders.RGB: 0,
-            HistogramOrders.RBG: 1,
-            HistogramOrders.GRB: 2,
-            HistogramOrders.GBR: 3,
-            HistogramOrders.BRG: 4,
-            HistogramOrders.BGR: 5,
-            HistogramOrders.Gray: 6
-    ]
-    
-    @IBAction func HandleProcessedHistogramChanged(_ sender: Any)
-    {
-        if let Switch = sender as? UISwitch
-        {
-            Settings.SetBoolean(Switch.isOn, ForKey: .ShowProcessedHistogram)
-        }
-    }
-    
-    @IBAction func HandleHistogramSpeedChanged(_ sender: Any)
-    {
-        if let Segment = sender as? UISegmentedControl
-        {
-            let NewIndex = Segment.selectedSegmentIndex
-            for (Speed, Index) in SpeedMap
-            {
-                if Index == NewIndex
-                {
-                    Settings.SetString(Speed.rawValue, ForKey: .HistogramCreationSpeed)
-                }
-            }
-        }
-    }
-    
-    func InitializeHistogramSpeed(_ Raw: String)
-    {
-        if let Speed = HistogramCreationSpeeds(rawValue: Raw)
-        {
-            HistogramSpeedSegment.selectedSegmentIndex = SpeedMap[Speed]!
-        }
-        else
-        {
-            HistogramSpeedSegment.selectedSegmentIndex = 2
-        }
-    }
-    
-    let SpeedMap =
-        [
-            HistogramCreationSpeeds.Fastest: 0,
-            HistogramCreationSpeeds.Fast: 1,
-            HistogramCreationSpeeds.Medium: 2,
-            HistogramCreationSpeeds.Slow: 3,
-            HistogramCreationSpeeds.Slowest: 4
-    ]
-    
     let GridTypeList = ["None", "Simple", "Cross Hairs", "Rule of Three", "Exotic", "Tight"]
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int
@@ -275,23 +157,9 @@ class ViewSettings: UITableViewController, UIPickerViewDelegate, UIPickerViewDat
         }
     }
     
-    @IBAction func HandleCombineChannelsChanged(_ sender: Any)
-    {
-        if let Switch = sender as? UISwitch
-        {
-            Settings.SetBoolean(Switch.isOn, ForKey: .CombinedHistogram)
-        }
-    }
-    
-    @IBOutlet weak var CombineHistogramColorsSwitch: UISwitch!
     @IBOutlet weak var GridPicker: UIPickerView!
     @IBOutlet weak var ShowCurrentOrientationSwitch: UISwitch!
-    @IBOutlet weak var HistogramSpeedSegment: UISegmentedControl!
-    @IBOutlet weak var ProcessedHistogramSwitch: UISwitch!
-    @IBOutlet weak var HistogramOrder: UISegmentedControl!
     @IBOutlet weak var BestFitOffsetSegment: UISegmentedControl!
     @IBOutlet weak var BestFitSwitch: UISwitch!
-    @IBOutlet weak var HistogramLabel: UILabel!
     @IBOutlet weak var ModeSegements: UISegmentedControl!
-    @IBOutlet weak var ShowHistogramSwitch: UISwitch!
 }
