b003dc20a0344fd6041b3ba061939e49869f68bb
Stuart Rankin <sjrankin@gmail.com>
2020-03-19 16:53:07 +0900

    Update Locations.swift
    Added conditions to run the location manager to help reduce battery usage.

--- BlockCam/+ViewController/Locations.swift
+++ BlockCam/+ViewController/Locations.swift
@@ -10,10 +10,16 @@ import Foundation
 import UIKit
 import CoreLocation
 
+/// Code to get the altitude and heading of the device for display on the HUD.
 extension ViewController: CLLocationManagerDelegate
 {
+    /// Initialize the system location manager. If `.ShowCompass` and `.ShowAltitude` are both false,
+    /// control returns immediately with no initialization. In this case, when the user changes either
+    /// setting, this function is called and initialization will cocur.
     func InitializeLocation()
     {
+        if Settings.GetBoolean(ForKey: .ShowCompass) || Settings.GetBoolean(ForKey: .ShowAltitude)
+        {
         self.LocationManager = CLLocationManager()
         LocationManager?.requestWhenInUseAuthorization()
         LocationManager?.delegate = self
@@ -24,22 +30,26 @@ extension ViewController: CLLocationManagerDelegate
         LocationManager?.startUpdatingHeading()
         UpdateLocation()
         StartLocationGathering()
+        }
     }
     
+    /// Starts a timer to read the altitude and header periodically.
     func StartLocationGathering()
     {
-        LocationTimer = Timer.scheduledTimer(timeInterval: 1.0,
+        LocationTimer = Timer.scheduledTimer(timeInterval: Settings.GetDouble(ForKey: .LocationUpdateFrequency),
                                              target: self,
                                              selector: #selector(UpdateLocation),
                                              userInfo: nil,
                                              repeats: true)
     }
     
+    /// Request the current heading and altitude.
     @objc func UpdateLocation()
     {
         LocationManager?.requestLocation()
     }
     
+    /// Called by the system location manager when a new header is available.
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading)
     {
         if Settings.GetBoolean(ForKey: .ShowCompass)
@@ -48,6 +58,7 @@ extension ViewController: CLLocationManagerDelegate
         }
     }
     
+    /// Called by the system location manager when a new location is available.
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
         if let Current = locations.first
@@ -61,8 +72,15 @@ extension ViewController: CLLocationManagerDelegate
         }
     }
     
+    /// Called by the system location manager when an error occurs.
+    /// When this happens, certain settings are reset to false and updating the heading and location
+    /// is turned off.
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
     {
-        fatalError("Location manager failed: \(error.localizedDescription)")
+        Log.Message("Location error: \(error.localizedDescription)")
+        Settings.SetBoolean(false, ForKey: .ShowAltitude)
+        Settings.SetBoolean(false, ForKey: .ShowCompass)
+        LocationManager?.stopUpdatingLocation()
+        LocationManager?.stopUpdatingHeading()
     }
 }
